a8dd35d3dba7c5c163fbfc6c414a9a61f6924809
positiveWand <singbhang@gmail.com>
2022-08-09 11:46:29 +0900

    회원 서비스 인터페이스 수정(Id driven)

--- src/main/java/com/cnu/sns/member/MemberService.java
+++ src/main/java/com/cnu/sns/member/MemberService.java
@@ -3,14 +3,14 @@ package com.cnu.sns.member;
 import java.util.List;
 
 public interface MemberService {
-    boolean joinMember(Member member);
+    boolean joinMember(Member newMember);
     Member getMember(Long memberId);
     boolean updateMember(Member member);
-    boolean deleteMember(Member member);
+    boolean deleteMember(Long memberId);
 
-    boolean followMember(Member follower, Member followee);
-    boolean unfollowMember(Member follower, Member followee);
+    boolean followMember(Long followerId, Long followeeId);
+    boolean unfollowMember(Long followerId, Long followeeId);
 
-    List<Member> getFollowers(Member member);
-    List<Member> getFollowees(Member member);
+    List<Member> getFollowers(Long memberId);
+    List<Member> getFollowees(Long memberId);
 }
