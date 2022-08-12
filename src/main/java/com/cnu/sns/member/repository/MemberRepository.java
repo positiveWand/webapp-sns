6f9ee8534d2ef25570e9515f436f56907a8d1caf
positiveWand <singbhang@gmail.com>
2022-08-12 15:28:12 +0900

    merge complete

--- src/main/java/com/cnu/sns/member/repository/MemberRepository.java
+++ src/main/java/com/cnu/sns/member/repository/MemberRepository.java
@@ -3,11 +3,14 @@ package com.cnu.sns.member.repository;
 import com.cnu.sns.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 
 public interface MemberRepository extends JpaRepository<Member, String> {
-    Member findById(String mid);
-    List<Member> findByMbirthdate(Character Gender);
-    List<Member> findByGender(Character Gender);
-    List<Member> findByMname(Character Gender);
-    List<Member> findByMemail(Character Gender);
+    Member findByMid(String mid);
+    List<Member> findByMbirthdate(LocalDate birthdate);
+    List<Member> findByGender(Character gender);
+    List<Member> findByMname(String name);
+    List<Member> findByMemail(String email);
 }
